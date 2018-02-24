1f6884c9507e896cca94a91f0ef5cf80aebf93b5
Sethupathi Sivasamy <ssethupathi@temenos.com>
2018-02-24 06:51:46 +0530

    java version and eclipse metadata changes

---
+++ MqJmsClient/src/main/java/com/temenos/test/JmsContext.java
@@ -0,0 +1,12 @@
+package com.temenos.test;
+
+import javax.jms.Session;
+
+public class JmsContext {
+
+	public Session newSession() {
+		return null;
+	}
+	
+	private static final String 
+}
---
+++ MqJmsClient/src/main/java/com/temenos/test/MessageLoader.java
@@ -0,0 +1,7 @@
+package com.temenos.test;
+
+public class MessageLoader {
+	
+	
+
+}
